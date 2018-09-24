68a6d551ea31c90a0452d63f3edbe2afe73fadc9
TheTBM <43273510+TheTBM@users.noreply.github.com>
2018-09-23 22:33:48 -0400

    Moving and Looking
    can move fine but can only look left and right

---
+++ Assets/Plugins/ControllerPluginWrapper.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace ControllerInputs
+{
+	public class ControllerPluginWrapper : MonoBehaviour
+	{
+		const string DLL_NAME = "ControllerPlugin";
+
+		[DllImport(DLL_NAME)]
+		public static extern void Initiate();
+
+		[DllImport(DLL_NAME)]
+		public static extern void UpdateControllers();
+
+		[DllImport(DLL_NAME)]
+		public static extern void RefreshStates();
+
+		[DllImport(DLL_NAME)]
+		public static extern bool LStick_InDeadZone(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool RStick_InDeadZone(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern float LeftStick_X(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern float LeftStick_Y(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern float RightStick_X(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern float RightStick_Y(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern float LeftTrigger(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern float RightTrigger(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool GetButtonPressed(int index, int button);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool GetButtonDown(int index, int button);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool GetPrevButtonDown(int index, int button);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool GetPrevLeftStickMoveX(int index, float threshold);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool GetPrevLeftStickMoveY(int index, float threshold);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool GetPrevRightStickMoveX(int index, float threshold);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool GetPrevRightStickMoveY(int index, float threshold);
+
+		[DllImport(DLL_NAME)]
+		public static extern int GetIndex(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern bool Connected(int index);
+
+		[DllImport(DLL_NAME)]
+		public static extern void Rumble(int index, float leftMotor = 0.0f, float rightMotor = 0.0f);
+	}
+}
---
+++ Assets/Scripts/PlayerMoving.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ControllerInputs;
+
+public class PlayerMoving : MonoBehaviour
+{
+	public float speed;
+	public float rotationSpeed;
+	public Camera camera;
+	Vector3 direction;
+	Vector3 lookDirection;
+
+	// Use this for initialization
+	void Start ()
+	{
+		direction = new Vector3(0, 0, 0);
+		ControllerPluginWrapper.Initiate();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		ControllerPluginWrapper.UpdateControllers();
+
+		direction = new Vector3(0, 0, 0);
+		lookDirection = new Vector3(0, 0, 0);
+
+		if (!ControllerPluginWrapper.LStick_InDeadZone(0))
+		{
+			direction.x = ControllerPluginWrapper.LeftStick_X(0);
+			direction.z = ControllerPluginWrapper.LeftStick_Y(0);
+		}
+
+		if (!ControllerPluginWrapper.RStick_InDeadZone(0))
+		{
+			lookDirection.x = ControllerPluginWrapper.RightStick_X(0);
+			lookDirection.z = ControllerPluginWrapper.RightStick_Y(0);
+		}
+
+		camera.transform.Translate(direction * speed * Time.deltaTime);
+		camera.transform.Rotate(Vector3.up * lookDirection.x * rotationSpeed);
+
+		ControllerPluginWrapper.RefreshStates();
+	}
+}
